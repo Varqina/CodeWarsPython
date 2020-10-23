adf3ae0c568cfed33bcc2455c70aab0af3276b87
Varqina <flisu2@gmail.com>
2020-10-23 13:31:53 +0200

    Url added

--- WriteNumberinExpandedForm6kyu.py
+++ WriteNumberinExpandedForm6kyu.py
@@ -3,7 +3,7 @@
 expanded_form(12) # Should return '10 + 2'
 expanded_form(42) # Should return '40 + 2'
 expanded_form(70304) # Should return '70000 + 300 + 4'
-
+https://www.codewars.com/kata/5842df8ccbd22792a4000245/train/python
 NOTE: All numbers will be whole numbers greater than 0."""
 
 def expanded_form(num):
